37d7f2912c8ee85357e34bb559cfa60868236fda
Walshy304 <walshda@mail.gvsu.edu>
2015-01-22 13:02:41 -0500

    Final version
    Only thing missing are a few comments.

--- SimpleShell.c
+++ SimpleShell.c
@@ -4,45 +4,89 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/resource.h>
 
-int main()
+int main(int argc, char* argv [])
 {
+    
+    
+    
+    
 	while(1)
 	{
-		char string[255];
-		char *command, *token;
-		char *params[255];
+		char string[1024];
+		char *command;
 		int status;
-		
-		fgets(string, sizeof(string), stdin);
-		command = malloc(sizeof(string) + 1);
+        pid_t pid;
+        int childSwitches = 0, totalSwitches = 0;
+        struct rusage usage;
+        
+        // Clock variable and time  -w
+        clock_t start, end;
+        double cpu_time_used;
+        
+        
+        fputs("<Lab2>", stdout);
+		fgets(string, 1024, stdin);
+        if (strcmp(string, "quit\n") == 0){
+            exit(0);
+        }
+
+        
+        int size = 10;
+        char** args = (char**) calloc(size, sizeof(char*) * size);
 		command = strtok(string, " ");
-		token = strtok(NULL, " ");
-		printf("%s\n", command);
+        
+        
 		int i = 0;
-		while(token != NULL)
+		while(1)
 		{
-			params[i] = malloc(sizeof(token) + 1);
-			strcpy(params[i], token);
-			token = strtok(NULL, " ");
-			printf("%s\n", params[i]);
+            if(i >=size ){
+                size = size * 2;
+                args = (char**) realloc(args, sizeof(char*) * size);
+            }
+			args[i] = malloc(sizeof(char) * strlen(command));
+			strcpy(args[i], command);
+			command = strtok(NULL, " ");
+            //printf("%s", params[i]);
+            //strtok(params[i], "\n");
+			//printf("%s\n", params[i]);
 			i++;
+            if(!command){
+                break;
+            }
 		}
-		
-		int pid = fork();
+        //removing the newline character
+        args[i-1] = strtok(args[i-1], "\n");
+        pid = fork();
+        start = clock();
+    
 		if(pid < 0)
 		{
-			printf("Error in fork");
+			perror("Error in fork");
+            exit(1);
 		}
 		else if(pid) // Parent
 		{
-			printf("%s\n", "Parent Waiting... ");
-			waitpid(-1, &status, 0);
+            wait(&status);
+            end = clock();
+            cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+            printf("%s", "Time used for child: ");
+            printf("%f seconds\n", cpu_time_used);
+            getrusage(RUSAGE_CHILDREN, &usage);
+            totalSwitches = usage.ru_nivcsw;
+            printf("%d contact switches made\n", totalSwitches-childSwitches);
+            childSwitches = totalSwitches;
 		}
 		else // Child
 		{
-			printf("%s\n", "Child Begins... ");
-			execve(command, params, 0);
+            if (execvp(*args, args)<0){
+                perror("execution of child failed");
+                exit(1);
+            }
+            return 0;
 		}
 	}
+    return 0;
 }
